1d3cb63094fdb186b99a7d6e50296b832e9905f8
mrjuarez1contactos <mrjuarez1contactos@gmail.com>
2025-12-13 19:30:33 -0700

    Fix CORS: allow all headers for multipart form-data requests

--- backend/main.py
+++ backend/main.py
@@ -63,8 +63,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization", "X-Requested-With"],
-)
+allow_headers=["*"],)
 
 # --- Models ---
 class DriveRequest(BaseModel):
